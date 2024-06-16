59b3da116c0490d1e491669fffe1e3d2f42d7930
Mintscandy <3199608937@qq.com>
2024-06-16 15:14:03 +0800

    master

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,80 @@
 <template>
-  <n-layout style="height: 100vh">
-    <n-layout-header style="height: 64px;display: flex;align-items: center;justify-content: center;" bordered>
-      <n-h3 style="margin: 0px;">CandyChat</n-h3>
-    </n-layout-header>
-    <n-layout position="absolute" style="top: 64px" has-sider>
-      <n-layout-sider :on-after-enter="handleAfterEnter" :on-after-leave="handleAfterLeave" collapse-mode="width"
-        :collapsed-width="20" :width="240" :show-collapsed-content="false" show-trigger="arrow-circle"
-        content-style="padding: 18px;" bordered>
-        <NewChatItemButton />
-        <div v-for="item in chatList">
-          <ChatListItem :title="item.title" :chatId="item.id" />
-        </div>
-      </n-layout-sider>
-      <RouterView />
-      <n-layout-footer id="custom_footer" ref="custom_footer" position="absolute"
-        style="left:240px;height: 64px;display: flex;align-items: center;justify-content: center;" bordered>
-        <n-input size="large" placeholder="请输入内容" style="width: 40%;text-align: left;" v-model:value="input_data"
-          @keyup.enter="handleRequest()">
-          <template #suffix>
-            <n-icon size="20">
-              <svg t="1714905595925" class="icon" viewBox="0 0 1024 1024" version="1.1"
-                xmlns="http://www.w3.org/2000/svg" p-id="12163" width="200" height="200">
-                <path
-                  d="M485.734 76.86c-235.218 0-425.578 165.593-425.578 369.859 0 125.328 72.735 236.765 182.61 304.828v114.531c0 15.469 10.828 24.75 23.218 24.75 4.641 0 9.297-1.531 12.375-3.094l116.063-77.375c29.422 6.188 58.812 7.75 89.765 7.75 235.22 0 425.563-165.593 425.563-369.859 1.566-205.797-188.778-371.39-424.016-371.39z m0 679.346c-26.312 0-52.625-3.094-78.921-7.75l-12.391-3.094c-7.735-1.53-15.469 0-23.219 4.657l-10.812 7.718-55.72 37.157V734.55c0-10.844-6.187-21.688-15.468-26.313l-15.484-9.28C177.766 641.674 120.516 547.3 120.516 448.252c0-170.219 162.5-307.937 363.671-307.937 201.188 0 363.657 137.718 363.657 307.937 1.565 168.672-162.466 307.953-362.11 307.953zM244.313 457.531c0 28.219 22.859 51.078 51.078 51.078 28.203 0 51.062-22.859 51.062-51.078 0-28.172-22.86-51.047-51.062-51.047-28.22 0-51.079 22.875-51.079 51.047z m193.453 0c0 28.219 22.859 51.078 51.062 51.078s51.078-22.859 51.078-51.078c0-28.172-22.875-51.047-51.078-51.047s-51.062 22.875-51.062 51.047z m187.234 0c0 28.219 22.875 51.078 51.063 51.078 28.218 0 51.093-22.859 51.093-51.078 0-28.172-22.875-51.047-51.093-51.047-28.188 0-51.063 22.875-51.063 51.047z m293.128 79.957c-0.912-1.152-1.872-2.256-2.816-3.408-6.656 25.28-16.16 49.632-28.128 72.752 0.496 1.12 1.12 2.192 1.6 3.312 8.096 18.624 12.496 38.752 12.496 59.744 0 55.664-32.16 108.688-86.064 140.848l-8.688 5.216c-5.216 2.592-8.72 8.688-8.72 14.784V864.64l-31.28-20.88-6.096-4.336c-4.336-2.592-8.688-3.472-13.024-2.592l-6.944 1.712c-14.816 2.656-29.6 4.336-44.336 4.336-35.44 0-68.816-7.84-97.936-21.504-28.912 6.752-58.912 11.008-89.808 12.464 48.4 41.712 115.008 67.504 188.592 67.504 19.504 0 38-0.976 56.496-4.848l73.056 48.688c1.936 0.976 4.848 1.936 7.744 1.936 7.808 0 14.624-5.84 14.624-15.568v-72.096c69.152-42.848 114.944-112.976 114.944-191.84 0-48.192-16.848-92.976-45.712-130.128z"
-                  fill="#727272" p-id="12164"></path>
-              </svg>
-            </n-icon>
-          </template>
-        </n-input>
-      </n-layout-footer>
+  <n-message-provider>
+    <n-layout style="height: 100vh">
+      <n-layout-header style="height: 64px;display: flex;align-items: center;justify-content: center;" bordered
+        v-if="HeaderisCollapsed">
+        <n-h3 style="margin: 0px;">CandyChat</n-h3>
+      </n-layout-header>
+      <!-- copyToClipboard -->
+      <n-layout position="absolute" :style="HeaderisCollapsed ? 'top: 64px;' : 'top: 0px;'" has-sider>
+        <n-layout-sider :on-after-enter="handleAfterEnter" :on-after-leave="handleAfterLeave" collapse-mode="width"
+          :collapsed-width="20" :width="240" :show-collapsed-content="false" show-trigger="arrow-circle"
+          content-style="padding: 18px;" bordered>
+          <NewChatItemButton />
+          <div v-for="item in chatList">
+            <ChatListItem :title="item.title" :chatId="item.id" />
+          </div>
+        </n-layout-sider>
+        <RouterView />
+        <n-layout-footer id="custom_footer" ref="custom_footer" position="absolute"
+          style="left:240px;height: 64px;display: flex;align-items: center;justify-content: center;" bordered>
+          <n-input :disabled="!isInputDisabled" size="large" placeholder="请输入内容" style="width: 40%;text-align: left;"
+            v-model:value="input_data" :status="isRequestError ? 'error' : 'success'" @keyup.enter="handleRequest()">
+            <template #suffix>
+              <n-icon size="20">
+                <svg t="1714905595925" class="icon" viewBox="0 0 1024 1024" version="1.1"
+                  xmlns="http://www.w3.org/2000/svg" p-id="12163" width="200" height="200">
+                  <path
+                    d="M485.734 76.86c-235.218 0-425.578 165.593-425.578 369.859 0 125.328 72.735 236.765 182.61 304.828v114.531c0 15.469 10.828 24.75 23.218 24.75 4.641 0 9.297-1.531 12.375-3.094l116.063-77.375c29.422 6.188 58.812 7.75 89.765 7.75 235.22 0 425.563-165.593 425.563-369.859 1.566-205.797-188.778-371.39-424.016-371.39z m0 679.346c-26.312 0-52.625-3.094-78.921-7.75l-12.391-3.094c-7.735-1.53-15.469 0-23.219 4.657l-10.812 7.718-55.72 37.157V734.55c0-10.844-6.187-21.688-15.468-26.313l-15.484-9.28C177.766 641.674 120.516 547.3 120.516 448.252c0-170.219 162.5-307.937 363.671-307.937 201.188 0 363.657 137.718 363.657 307.937 1.565 168.672-162.466 307.953-362.11 307.953zM244.313 457.531c0 28.219 22.859 51.078 51.078 51.078 28.203 0 51.062-22.859 51.062-51.078 0-28.172-22.86-51.047-51.062-51.047-28.22 0-51.079 22.875-51.079 51.047z m193.453 0c0 28.219 22.859 51.078 51.062 51.078s51.078-22.859 51.078-51.078c0-28.172-22.875-51.047-51.078-51.047s-51.062 22.875-51.062 51.047z m187.234 0c0 28.219 22.875 51.078 51.063 51.078 28.218 0 51.093-22.859 51.093-51.078 0-28.172-22.875-51.047-51.093-51.047-28.188 0-51.063 22.875-51.063 51.047z m293.128 79.957c-0.912-1.152-1.872-2.256-2.816-3.408-6.656 25.28-16.16 49.632-28.128 72.752 0.496 1.12 1.12 2.192 1.6 3.312 8.096 18.624 12.496 38.752 12.496 59.744 0 55.664-32.16 108.688-86.064 140.848l-8.688 5.216c-5.216 2.592-8.72 8.688-8.72 14.784V864.64l-31.28-20.88-6.096-4.336c-4.336-2.592-8.688-3.472-13.024-2.592l-6.944 1.712c-14.816 2.656-29.6 4.336-44.336 4.336-35.44 0-68.816-7.84-97.936-21.504-28.912 6.752-58.912 11.008-89.808 12.464 48.4 41.712 115.008 67.504 188.592 67.504 19.504 0 38-0.976 56.496-4.848l73.056 48.688c1.936 0.976 4.848 1.936 7.744 1.936 7.808 0 14.624-5.84 14.624-15.568v-72.096c69.152-42.848 114.944-112.976 114.944-191.84 0-48.192-16.848-92.976-45.712-130.128z"
+                    fill="#727272" p-id="12164"></path>
+                </svg>
+              </n-icon>
+            </template>
+          </n-input>
+          <n-popselect style="margin-left: auto !important;" v-model:value="value" :options="ModelOptions"
+            trigger="click" @update:value="changeCurrentModel">
+            <n-button>{{ currentModel }}</n-button>
+          </n-popselect>
+        </n-layout-footer>
+      </n-layout>
     </n-layout>
-  </n-layout>
+  </n-message-provider>
 </template>
 <script setup>
 import { ref, onMounted, reactive } from 'vue';
 import ChatListItem from '@/components/ChatListItem.vue';
 import { RouterView } from 'vue-router';
-import { watch } from 'vue';
 import { useRoute } from 'vue-router';
 var axios = require('axios');
 import NewChatItemButton from '@/components/NewChatItemButton.vue';
 const route = useRoute();
 const custom_footer = ref(null);
 const input_data = ref('');
+const isInputDisabled = ref(true);
+const isRequestError = ref(false);
+const HeaderisCollapsed = ref(true);
+const currentModel = ref('gpt-3.5-turbo-1106');
+const ModelOptions = ref([{
+  label: 'gpt-3.5-turbo-1106',
+  value: 'gpt-3.5-turbo-1106'
+},
+{
+  label: 'gpt-3.5-turbo-0125',
+  value: 'gpt-3.5-turbo-0125'
+},
+{
+  label: 'gpt-3.5-turbo',
+  value: 'gpt-3.5-turbo'
+},
+{
+  label: 'gpt-4o',
+  value: 'gpt-4o'
+},
+{
+  label: 'gpt-4o-2024-05-13',
+  value: 'gpt-4o-2024-05-13'
+}
+]);
 
 // 渲染localStorage中的聊天列表
 const chatList = ref([]);
@@ -59,10 +91,20 @@ const handleAfterLeave = () => {
   // 当侧边栏收起后，将底部栏向左移动
   custom_footer.value.$el.style.left = '20px';
 };
+
+const isMobile = () => {
+  let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i);
+  return flag;
+}
+
+const changeCurrentModel = (value, option) => {
+  currentModel.value = value;
+}
+
 // 获取接口回复
 const getChatData = async () => {
   var send_data = JSON.stringify({
-    "model": "gpt-3.5-turbo",
+    "model": `${currentModel.value}`,
     "messages": [
       {
         "role": "user",
@@ -92,10 +134,13 @@ const getChatData = async () => {
     return null; // 返回null表示出错
   }
 };
+
 const handleRequest = async () => {
   // 处理页面请求
   console.log(input_data.value);
 
+  // 禁用输入框，防止重复发送
+  isInputDisabled.value = false
   // 等待getChatData函数执行完毕并获取返回值
   let message = await getChatData();
 
@@ -118,12 +163,20 @@ const handleRequest = async () => {
     window.localStorage.setItem(route.query.id, JSON.stringify(pre_data));
 
     // 发送事件
-    // ...
     const event = new CustomEvent('updateChatView', { detail: pre_data });
     window.dispatchEvent(event);
     input_data.value = ""
+    isInputDisabled.value = true
   } else {
     console.log('Failed to get chat data');
+    // 提示用户 请求失败
+    isRequestError.value = true
+    // 3秒后恢复
+    setTimeout(() => {
+      isRequestError.value = false
+      input_data.value = ""
+    }, 3000);
+    isInputDisabled.value = true
   }
 };
 
@@ -131,6 +184,9 @@ const handleRequest = async () => {
 onMounted(() => {
   // Code to run after component is mounted
   custom_footer.value.$el.style.left = '240px';
+  if (isMobile()) {
+    HeaderisCollapsed.value = false;
+  }
 });
 </script>
 <style lang="scss">
--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,75 +1,134 @@
 <template>
     <n-layout id="chatview" content-style="padding: 6%; padding-left: 15%; padding-right: 15%" :native-scrollbar="false"
-        @click="contentRef?.scrollTo({ top: 200, behavior: 'smooth' })" ref="contentRef" style="bottom: 64px;">
-        <div v-for="item in MessageList" style="margin-bottom: 10px;">
-            <n-card :title="item.role" width hoverable size="small">
+        ref="contentRef" style="bottom: 64px;margin-top: 50px;">
+        <div v-for="item in MessageList" :key="item.id" style="margin-bottom: 2%;">
+            <n-card :title="item.role" width hoverable size="small" style="padding: 1%;">
                 <n-card-content>
-                    <div v-html="markdown.render(item.content) "></div>
-                    <!-- <vue-markdown :source="item.content"></vue-markdown> -->
-                    <!-- <p></p> -->
+                    <div v-html="renderMarkdown(item.content)"></div>
                 </n-card-content>
+                <template #footer v-if='item.role != "你 🤔"'>
+                    <div class="copy_button" @click="copyToClipboard(item.content)">
+                        <n-icon size="15" class="copy_icon_">
+                            <svg t="1718343191961" class="icon" viewBox="0 0 1024 1024" version="1.1"
+                                xmlns="http://www.w3.org/2000/svg" p-id="6898" width="200" height="200">
+                                <path
+                                    d="M407.424 53.312H403.2c-68.352 0-122.56 0-165.376 5.12-43.968 5.12-80.192 16.064-110.464 40.96-10.24 8.32-19.648 17.792-28.096 28.032-24.832 30.272-35.712 66.432-40.96 110.464C53.312 280.768 53.312 334.912 53.312 403.2v4.16c0 68.352 0 122.496 5.12 165.376 5.12 43.968 16.064 80.192 40.96 110.464 8.32 10.24 17.792 19.648 28.032 28.032 28.672 23.552 62.72 34.56 103.68 40.128 40.064 5.376 90.112 5.824 152.448 5.888H384a32.128 32.128 0 0 0 32-32.832V597.312a32 32 0 0 0-64 0v95.936c-47.36-0.32-83.392-1.344-112.384-5.248-34.496-4.672-55.488-12.928-71.616-26.176-7.04-5.76-13.44-12.16-19.2-19.2-13.888-16.96-22.4-39.424-26.88-77.376-4.48-38.656-4.608-88.96-4.608-159.936 0-70.912 0.064-121.28 4.672-159.936 4.48-37.952 12.928-60.416 26.88-77.376 5.76-7.04 12.16-13.44 19.2-19.2 16.896-13.888 39.36-22.4 77.312-26.88 38.656-4.48 89.024-4.608 160-4.608 70.848 0 121.28 0.064 159.872 4.672 37.952 4.48 60.416 12.928 77.44 26.88 6.976 5.76 13.44 12.16 19.2 19.2 13.184 16 21.44 37.12 26.112 71.552 3.84 28.992 4.928 65.024 5.248 112.384H597.312a32 32 0 0 0 0 64h128.064a32 32 0 0 0 32-32c-0.128-62.592-0.512-112.704-5.952-152.96-5.504-40.96-16.576-74.944-40.128-103.616a202.88 202.88 0 0 0-28.032-28.096c-30.272-24.832-66.496-35.712-110.464-40.96C529.92 53.312 475.776 53.312 407.424 53.312z"
+                                    fill="#333333" p-id="6899"></path>
+                                <path
+                                    d="M493.12 415.744a32 32 0 1 0-8.32-63.488c-34.816 4.544-64.512 13.696-88.896 33.728-24.704 20.352-37.12 46.272-43.264 77.056a32 32 0 1 0 62.72 12.608c4.096-20.48 10.752-31.616 21.184-40.192 11.328-9.344 27.52-15.936 56.576-19.712zM837.824 352.256a32 32 0 1 0-8.32 63.488c29.056 3.84 45.248 10.368 56.576 19.712 10.432 8.576 17.088 19.712 21.184 40.192a32 32 0 1 0 62.72-12.608c-6.144-30.72-18.56-56.704-43.264-77.056-24.384-20.032-54.08-29.184-88.896-33.728zM970.624 597.312a32 32 0 0 0-64 0v128a32 32 0 0 0 64 0v-128zM415.36 847.04a32 32 0 0 0-62.72 12.608c6.144 30.72 18.56 56.64 43.264 76.992 24.384 20.096 54.08 29.184 88.96 33.728a32 32 0 0 0 8.32-63.424c-29.056-3.84-45.312-10.368-56.64-19.712-10.432-8.576-17.088-19.712-21.12-40.192zM970.048 859.648a32 32 0 1 0-62.72-12.608c-4.16 20.48-10.816 31.616-21.248 40.192-11.328 9.344-27.52 15.872-56.576 19.712a32 32 0 0 0 8.32 63.424c34.816-4.48 64.512-13.632 88.96-33.728 24.704-20.352 37.12-46.272 43.264-76.992zM597.312 906.688a32 32 0 0 0 0 64h128a32 32 0 0 0 0-64h-128z"
+                                    fill="#333333" p-id="6900"></path>
+                            </svg>
+                        </n-icon>
+                    </div>
+                </template>
             </n-card>
         </div>
     </n-layout>
 </template>
+
 <script setup>
 import { useRoute } from 'vue-router';
-import { ref, reactive, onMounted } from 'vue';
-import { watch } from 'vue';
+import { useMessage } from 'naive-ui';
+import { ref, onMounted, watch, nextTick } from 'vue';
 import MarkdownIt from 'markdown-it';
-const markdown = new MarkdownIt()
+import hljs from 'highlight.js';
+import 'highlight.js/styles/github-dark.css';
+import markdownItHighlightjs from 'markdown-it-highlightjs';
+
+// Configure MarkdownIt with highlight.js
+const markdown = new MarkdownIt().use(markdownItHighlightjs, { hljs });
 
 const contentRef = ref(null);
-
-
 const route = useRoute();
-const id = ref();
 const MessageList = ref([]);
-
+const message = useMessage();
 
 onMounted(() => {
     updateData();
-    // 滚动到底部
-    // console.dir(contentRef.value.$el.clientHeight);
-
-    // 渲染当前id的聊天记录
     window.addEventListener('updateChatView', updateData);
+    nextTick(() => {
+        scrollToBottom();
+    });
 });
 
 watch(() => route.query.id, () => {
     MessageList.value = [];
-    console.log('watch');
-    updateData()
+    updateData();
+    nextTick(() => {
+        scrollToBottom();
+    });
 });
 
 function updateData() {
-    // console.log('updateData');
-    // console.log(route.query.id);
     let id = route.query.id;
     for (let i = 0; i < localStorage.length; i++) {
         let key = localStorage.key(i);
-        // 得到当前id的JSON数据
         let item = JSON.parse(localStorage.getItem(key));
         console.log(item);
         if (item.id === id) {
             MessageList.value = item.messages;
         }
     }
-    // MessageList = reactive(currentChat);
+    scrollToBottom();
+}
+
+function renderMarkdown(content) {
+    return markdown.render(content);
+}
+
+function scrollToBottom() {
+    nextTick(() => {
+        const container = contentRef.value?.$el;
+        if (container) {
+            container.scrollTop = container.scrollHeight;
+        }
+    });
+}
+
+function copyToClipboard(content) {
+    // 粘贴到剪贴板
+    navigator.clipboard.writeText(content);
+    // 提示
+    message.success('已复制到剪贴板');
 }
 </script>
+
 <style lang="scss">
+.n-card__footer {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 5px;
+}
+
+.copy_button {
+    cursor: pointer;
+}
+
+.copy_icon_ {
+    padding: 3px !important;
+    border-radius: 25%;
+}
+
+.copy_icon_:hover {
+    background-color: #e8e8e8;
+}
+
 body {
     font-family: Quotes, sans-serif;
 }
 
 .n-card-header {
-    // 不居中
     text-align: left;
 }
 
 .n-card__content {
-    // 不居中
     text-align: left;
+}
+
+pre code {
+    background: #f5f5f5;
+    padding: 10px;
+    border-radius: 5px;
 }
 </style>
